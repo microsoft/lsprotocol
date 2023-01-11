be9c21527451f144763e4d3f1b079b5259a0ee62
Karthik Nadig <kanadig@microsoft.com>
2023-01-10 21:34:01 -0800

    Ensure version matches the year.

---
+++ .github/workflows/version_check.py
@@ -0,0 +1,18 @@
+import datetime
+import pathlib
+import sys
+
+pyproject = pathlib.Path(__file__).parent.parent.parent / "pyproject.toml"
+
+content = pyproject.read_text(encoding="utf-8")
+
+for line in content.splitlines():
+    if line.startswith("version"):
+        version = line.split("=")[1].strip().strip('"')
+        year, minor, micro = version.split(".")
+        today = datetime.date.today()
+        if int(year) != today.year:
+            print(f"Version {version} year should be {today.year}")
+            sys.exit(1)
+
+print("Version check passed")

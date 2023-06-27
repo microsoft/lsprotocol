1295ec27a340cbca8aadc7ac36c1c986d8b29a66
Karthik Nadig <kanadig@microsoft.com>
2023-06-27 15:47:10 -0700

    dotnet: Add test case generation  (#226)
    For: https://github.com/microsoft/lsprotocol/issues/183

---
+++ generator-plugins/testdata/testdata_utils.py
@@ -0,0 +1,23 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import logging
+import pathlib
+from typing import Dict, List
+
+import generator.model as model
+
+from .testdata_generator import generate
+
+logger = logging.getLogger("testdata")
+
+
+def generate_from_spec(spec: model.LSPModel, output_dir: str) -> None:
+    """Generate the code for the given spec."""
+    output = pathlib.Path(output_dir)
+    # key is the relative path to the file, value is the content
+    code: Dict[str, str] = generate(spec, logger)
+    for file_name in code:
+        # print file size
+        file = output / file_name
+        file.write_text(code[file_name], encoding="utf-8")
--- tests/dotnet/lsprotocol_tests/LSPTests.cs
+++ tests/dotnet/lsprotocol_tests/LSPTests.cs
@@ -3,11 +3,13 @@ namespace lsprotocol_tests;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
+
 public class LSPTests
 {
     public static IEnumerable<object[]> JsonTestData()
     {
-        string folderPath = Environment.GetEnvironmentVariable("JSON_FOLDER_PATH");
+        string folderPath = "C:\\GIT\\LSP\\lsprotocol\\packages\\testdata";
+
         string[] jsonFiles = Directory.GetFiles(folderPath, "*.json");
         foreach (string filePath in jsonFiles)
         {
@@ -22,17 +24,33 @@ public class LSPTests
         string original = File.ReadAllText(filePath);
 
         // Get the class name from the file name
-        // format: <class-name>_<test-id>.json
+        // format: <class-name>-<valid>-<test-id>.json
+        // classname => Class name of the type to deserialize to
+        // valid => true if the file is valid, false if it is invalid
+        // test-id => unique id for the test
         string fileName = Path.GetFileNameWithoutExtension(filePath);
-        string[] nameParts = fileName.Split('_');
+        string[] nameParts = fileName.Split('-');
         string className = nameParts[0];
+        bool valid = nameParts[1] == "True";
 
-        Type type = Type.GetType(className) ?? throw new Exception($"Type {className} not found");
-        object? deserializedObject = JsonConvert.DeserializeObject(original, type);
-        string newJson = JsonConvert.SerializeObject(deserializedObject);
+        Type type = Type.GetType($"Microsoft.LanguageServer.Protocol.{className}, lsprotocol") ?? throw new Exception($"Type {className} not found");
+        RunTest(valid, original, type);
+    }
 
-        JToken token1 = JToken.Parse(original);
-        JToken token2 = JToken.Parse(newJson);
-        Assert.True(JToken.DeepEquals(token1, token2));
+    private static void RunTest(bool valid, string data, Type type)
+    {
+        if (valid)
+        {
+            object? deserializedObject = JsonConvert.DeserializeObject(data, type);
+            string newJson = JsonConvert.SerializeObject(deserializedObject);
+
+            JToken token1 = JToken.Parse(data);
+            JToken token2 = JToken.Parse(newJson);
+            Assert.True(JToken.DeepEquals(token1, token2));
+        }
+        else
+        {
+            Assert.Throws<Exception>(() => JsonConvert.DeserializeObject(data, type));
+        }
     }
 }

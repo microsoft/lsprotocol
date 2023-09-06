a67c2f8fbfdc0190027d314c24e215097f44ca7b
Anthony Kim <anthonykim@microsoft.com>
2023-09-06 14:40:10 -0700

    add install for Rust and Cargo

--- scripts/onCreateCommand.sh
+++ scripts/onCreateCommand.sh
@@ -4,12 +4,15 @@
 curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-# echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 # eval "$(pyenv init -)" Comment this out and DO NOT use shim.
 source ~/.bashrc
+
+# Install Rust and Cargo
+curl https://sh.rustup.rs -sSf | bash -s -- -y
+echo 'source $HOME/.cargo/env' >> ~/.bashrc
 
 # Install Python via pyenv .
 pyenv install 3.8:latest 3.9:latest 3.10:latest 3.11:latest

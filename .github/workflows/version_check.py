ebcacd8c6857431f96741fb51d534ea2f877e5ce
Karthik Nadig <kanadig@microsoft.com>
2023-11-06 13:37:42 -0800

    make year check flexible for new releases

--- .github/workflows/version_check.py
+++ .github/workflows/version_check.py
@@ -20,8 +20,8 @@ for line in content.splitlines():
         version = line.split("=")[1].strip().strip('"')
         year, minor, micro = version.split(".")
         today = datetime.date.today()
-        if int(year) != today.year:
-            print(f"Version {version} year should be {today.year}")
+        if int(year) in [today.year, today.year + 1]:
+            print(f"Version {version} year should be {today.year} or {today.year + 1}")
             sys.exit(1)
 
 print("Version check passed")

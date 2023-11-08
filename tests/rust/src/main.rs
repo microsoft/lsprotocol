1c4ce2c3eb69bd071e15b530b693f871000aecd9
Karthik Nadig <kanadig@microsoft.com>
2023-11-08 15:18:20 -0800

    Get rust tests running

--- tests/rust/src/main.rs
+++ tests/rust/src/main.rs
@@ -1,57 +1,78 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-use glob::glob;
-use lsprotocol::*;
-use serde_json::Value;
-use std::fs;
-use std::path::Path;
-use std::str::FromStr;
 
-const TEST_DATA_ROOT: &str = std::env::var("TEST_DATA_ROOT").unwrap().as_str();
+#[cfg(test)]
+mod tests {
+    use glob::glob;
+    use lsprotocol::*;
+    use serde::Deserialize;
+    use serde_json::Value;
+    use std::fs;
 
-fn get_all_json_files(root: &str) -> Vec<String> {
-    let pattern = format!("{}/**/*.json", root);
-    glob(&pattern)
-        .expect("Failed to read glob pattern")
-        .filter_map(Result::ok)
-        .map(|path| path.to_str().unwrap().to_string())
-        .collect()
-}
-
-fn validate_type<T>(data: &Value) {
-    match serde_json::from_value::<T>(data.clone()) {
-        Ok(_) => assert_eq!(
-            result_type, "True",
-            "Expected error, but succeeded structuring"
-        ),
-        Err(_) => assert_eq!(
-            result_type, "False",
-            "Expected success, but failed structuring"
-        ),
+    fn get_all_json_files(root: &str) -> Vec<String> {
+        let pattern = format!("{}/*.json", root);
+        glob(&pattern)
+            .expect("Failed to read glob pattern")
+            .filter_map(Result::ok)
+            .map(|path| path.to_str().unwrap().to_string())
+            .collect()
     }
-}
 
-fn validate(lsp_type: &str, data: &Value) {
-    match lsp_type {
-        "CallHierarchyIncomingCallsRequest" => {
-            validate_type::<CallHierarchyIncomingCallsRequest>(data)
+    fn validate_type<T: for<'de> Deserialize<'de>>(result_type: &str, data: &Value) {
+        match serde_json::from_value::<T>(data.clone()) {
+            Ok(_) => assert_eq!(
+                result_type, "True",
+                "Expected error, but succeeded deserializing"
+            ),
+            Err(e) => assert_eq!(
+                result_type, "False",
+                "Expected success, but failed deserializing: {e}",
+            ),
         }
-        _ => (),
+    }
+
+    fn validate(lsp_type: &str, result_type: &str, data: &Value) {
+        match lsp_type {
+            "CallHierarchyIncomingCallsRequest" => {
+                return validate_type::<CallHierarchyIncomingCallsRequest>(result_type, data)
+            }
+
+            _ => (),
+        }
+    }
+
+    #[test]
+
+    fn test_generated_data() {
+        println!("Running generated data tests");
+        let cwd = std::env::current_dir()
+            .unwrap()
+            .join("../../packages/testdata");
+        let env_value =
+            std::env::var("TEST_DATA_ROOT").unwrap_or_else(|_| cwd.to_str().unwrap().to_string());
+        println!("TEST_DATA_ROOT: {}", env_value);
+        for json_file in get_all_json_files(env_value.as_str()) {
+            let basename = std::path::Path::new(&json_file)
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap();
+            let type_name_result_type: Vec<&str> = basename.split("-").collect();
+            let lsp_type = type_name_result_type[0];
+            let result_type = type_name_result_type[1];
+            println!("Validating: {}", json_file);
+            let data: Value =
+                serde_json::from_str(&fs::read_to_string(json_file.clone()).unwrap()).unwrap();
+
+            validate(&lsp_type, &result_type, &data);
+        }
     }
 }
 
-fn test_generated_data(json_file: &str) {
-    let type_name_result_type: Vec<&str> = json_file.split("-").collect();
-    let lsp_type = type_name_result_type[0];
-    let result_type = type_name_result_type[1];
-
-    let data: Value = serde_json::from_str(&fs::read_to_string(json_file).unwrap()).unwrap();
-
-    validate(&lsp_type, &data);
-}
-
+use lsprotocol::*;
+use std::fs;
 fn main() {
-    for json_file in get_all_json_files(TEST_DATA_ROOT) {
-        test_generated_data(&json_file);
-    }
+    // let file_path: String = "".to_string();
+    // let data = &fs::read_to_string(file_path.clone()).unwrap();
+    // let result: CallHierarchyIncomingCallsRequest = serde_json::from_str(data).unwrap();
 }
